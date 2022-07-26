bf258c15d0867d0fdaac8aa116a8cda47378d6c4
Surabhi951 <surabhibhagat95@gmail.com>
2022-07-27 00:38:23 +0530

    added elements in linkedlist

--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -9,5 +9,12 @@ public class LinkedListMain {
         linkedList1.push(56);
         linkedList1.print();
 
+        System.out.println();
+        LinkedList<Integer> linkedList2 = new LinkedList<>();
+        linkedList2.add(56);
+        linkedList2.add(30);
+        linkedList2.add(70);
+        linkedList1.print();
+
     }
 }
