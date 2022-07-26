6d5b0d7088cf41277df6a58d0623f6d8f83ade1d
Surabhi951 <surabhibhagat95@gmail.com>
2022-07-27 01:46:32 +0530

    inserting 30 between 56 and 70

--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -5,16 +5,16 @@ public class LinkedListMain {
         LinkedList<Integer> linkedList1 = new LinkedList<>();
 
         linkedList1.push(70);
-        linkedList1.push(30);
+//        linkedList1.push(30);
         linkedList1.push(56);
+       linkedList1.insert(30);
         linkedList1.print();
 
-        System.out.println();
+/*        System.out.println();
         LinkedList<Integer> linkedList2 = new LinkedList<>();
-        linkedList2.add(56);
-        linkedList2.add(30);
+        linkedList2.add(56);linkedList2.add(30);
         linkedList2.add(70);
         linkedList1.print();
-
+*/
     }
 }
