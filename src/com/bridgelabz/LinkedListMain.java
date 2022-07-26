18ebf835585f08a0307c1fb34b78d67fa87a71eb
Surabhi951 <surabhibhagat95@gmail.com>
2022-07-27 01:58:50 +0530

    Delete first element in linkedlist

--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -8,13 +8,15 @@ public class LinkedListMain {
 //        linkedList1.push(30);
         linkedList1.push(56);
        linkedList1.insert(30);
+        System.out.println(linkedList1.pop());
         linkedList1.print();
 
-/*        System.out.println();
+
+        System.out.println();
         LinkedList<Integer> linkedList2 = new LinkedList<>();
         linkedList2.add(56);linkedList2.add(30);
         linkedList2.add(70);
         linkedList1.print();
-*/
+
     }
 }
