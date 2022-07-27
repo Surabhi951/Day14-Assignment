2189ce3978a2eb594093232976e192a038940d46
Surabhi951 <surabhibhagat95@gmail.com>
2022-07-28 02:07:21 +0530

    Added created stack

---
+++ src/com/bridgelabz/Stack.java
@@ -0,0 +1,20 @@
+package com.bridgelabz;
+
+public class Stack<T extends Comparable<T>> {
+
+    LinkedList<T> linkedList;
+
+    public Stack() {
+        this.linkedList = new LinkedList<>();
+    }
+
+
+    public void push(T key) {
+        linkedList.push(key);
+    }
+
+    void print(){
+        linkedList.print();
+    }
+
+}
